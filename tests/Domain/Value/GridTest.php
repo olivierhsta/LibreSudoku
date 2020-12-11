65e156727f23515c14a308fa98f313f63aa88115
Olivier Hassaoui St-Amour <olivier@hassaoui.com>
2020-12-10 23:33:10 -0500

    Test buddies algorithm

--- tests/Domain/Value/GridTest.php
+++ tests/Domain/Value/GridTest.php
@@ -73,4 +73,57 @@ class GridTest extends TestCase
             ],
         ];
     }
+
+    /** @dataProvider buddiesProvider */
+    public function test_buddies(Grid $grid, int $cellKey, array $expectedBuddies)
+    {
+        $actualBuddies = $grid->getBuddiesOf($grid->getCell($cellKey));
+        $this->assertEquals(count($expectedBuddies), count($actualBuddies));
+        foreach ($actualBuddies as $buddy) {
+            $this->assertContains($buddy->key(), array_keys($expectedBuddies));
+            $this->assertContains($buddy->getValue(), array_values($expectedBuddies));
+        }
+    }
+
+    public function buddiesProvider(): array
+    {
+        /*
+        2,0,0 | 3,6,4 | 0,0,0
+        0,7,0 | 0,5,0 | 0,0,6
+        5,4,0 | 0,0,0 | 0,0,3
+        ---------------------
+        0,0,7 | 8,0,0 | 3,0,0
+        0,5,9 | 0,0,0 | 7,1,0
+        0,0,1 | 0,0,5 | 9,0,0
+        ---------------------
+        7,0,0 | 0,0,0 | 0,2,8
+        6,0,0 | 0,7,0 | 0,5,0
+        0,0,0 | 6,2,1 | 0,0,7
+         */
+        $grid = GridFactory::new()->createFromEncoding([
+            2,0,0,0,7,0,5,4,0,3,6,4,0,5,0,0,0,0,0,0,0,0,0,6,0,0,3,
+            0,0,7,0,5,9,0,0,1,8,0,0,0,0,0,0,0,5,3,0,0,7,1,0,9,0,0,
+            7,0,0,6,0,0,0,0,0,0,0,0,0,7,0,6,2,1,0,2,8,0,5,0,0,0,7,
+        ]);
+        return [
+            [
+                $grid,
+                0,
+                [
+                    1 => 0, 2 => 0, 3 => 0, 4 => 7, 5 => 0, 6 => 5, 7 => 4, 8 => 0, // box
+                    9 => 3, 10 => 6, 11 => 4, 18 => 0, 19 => 0, 20 => 0, // row
+                    27 => 0, 30 => 0, 33 => 0, 54 => 7, 57 => 6, 60 => 0 // column
+                ]
+            ],
+            [
+                $grid,
+                40,
+                [
+                    36 => 8, 37 => 0, 38 => 0, 39 => 0, 41 => 0, 42 => 0, 43 => 0, 44 => 5, // box
+                    30 => 0, 31 => 5, 32 => 9, 48 => 7, 49 => 1, 50 => 0, // row
+                    10 => 6, 13 => 5, 16 => 0, 64 => 0, 67 => 7, 70 => 2, // column
+                ]
+            ]
+        ];
+    }
 }
